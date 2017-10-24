d583cb4cc36d85510c77d26c057d048e3deab441
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2017-10-25 00:33:47 +0200

    commit part 6

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	void Start () {
+        Text finalScore = GetComponent<Text>();
+        finalScore.text = ScoreKeeper.score.ToString();
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public static int score = 0;
+    private Text scoreUi;
+
+	void Start()
+    {
+        scoreUi = GetComponent<Text>();
+        Reset();
+    }
+	
+    public void Score(int points)
+    {
+        score = score + points;
+        Debug.Log("Total points: " + score);
+        scoreUi.text = score.ToString();
+    }
+
+    public static void Reset()
+    {
+        score = 0;
+    }
+
+}
